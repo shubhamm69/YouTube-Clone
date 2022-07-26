344a863e4a90dc1eb0aaa6c128f28bcf6da3db36
shubhamm69 <shubham212a@gmail.com>
2022-07-26 17:13:21 +0530

    updated subcription sidebar

---
+++ script.js
@@ -0,0 +1,6 @@
+var menuicon = document.querySelector(".menu");
+var sidebar = document.querySelector(".sidebar");
+
+menuicon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar")
+}
